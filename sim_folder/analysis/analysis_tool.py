bc7efd5db61ee9c02e220098e02cc93abcd343b0
Remi Lehe <remi.lehe@normalesup.org>
2025-04-29 14:41:57 -0700

    Add analysis post-processing for temperatures

--- sim_folder/analysis/analysis_tool.py
+++ sim_folder/analysis/analysis_tool.py
@@ -4,6 +4,10 @@ import numpy as np
 import yt
 import tqdm
 yt.set_log_level("error")
+from scipy.constants import m_p, k
+# Convert constants to cgs
+m_H = m_p * 1e3 # g
+kB = k * 1e7 # erg/K
 
 class CastroSimulation(object):
     """
@@ -33,31 +37,58 @@ class CastroSimulation(object):
 
         Parameters:
         -----------
-        TODO
-        quantity: possible values: 'density', 'rho_Hn', 'rho_Hp', 'pressure', 'Temp'
-
-        Note: actually returns at a different time
+        t: float
+            time at which to extract the quantity
+        quantity: possible values: 'density', 'rho_Hn', 'rho_Hp', 'pressure', 'Temp', 'T_e', 'T_h'
+            quantity to extract
+        level: int
+            refinement level at which to extract the quantity
         """
         i_output = np.argmin( abs(t-self.output_times) )
         ds = self.ts[i_output]
-        if ds.dimensionality==1:
-            ad = ds.covering_grid( level=level,
+        if quantity in ['T_e', 'T_h']:
+            r, f = _extract_radius_and_quantity( ds, 'f_heavies', level )
+            r, e = _extract_radius_and_quantity( ds, 'eint_e', level )
+            r, X_H = _extract_radius_and_quantity( ds, 'X(Hp)', level )
+            if quantity == 'T_e':
+                q = 2*m_H*e*(1-f)/(3*X_H*kB)
+            else:
+                q = 2*m_H*e*f/(3*kB)
+        else:
+            r, q = _extract_radius_and_quantity( ds, quantity, level )
+        return r, q, ds.current_time.to_value()
+
+def _extract_radius_and_quantity( ds, quantity, level ):
+    """
+    Extract the quantity `quantity` at the required refinement level
+
+    Parameters:
+    -----------
+    ds: yt.Dataset
+        dataset to extract the quantity from
+    quantity: string
+        quantity to extract
+    level: int
+        refinement level at which to extract the quantity
+    """
+    if ds.dimensionality==1:
+        ad = ds.covering_grid( level=level,
+                        left_edge=ds.domain_left_edge,
+                        dims=[ds.domain_dimensions[0]*2**level, 1, 1] )
+        q = ad[quantity].to_ndarray().squeeze()
+        r = np.linspace(
+            ds.domain_left_edge[0],
+            ds.domain_right_edge[0],
+            ds.domain_dimensions[0]*2**level)
+    elif ds.dimensionality:
+        ad = ds.covering_grid( level=level,
                             left_edge=ds.domain_left_edge,
-                            dims=[ds.domain_dimensions[0]*2**level, 1, 1] )
-            q = ad[quantity].to_ndarray().squeeze()
-            r = np.linspace(
-                ds.domain_left_edge[0],
-                ds.domain_right_edge[0],
-                ds.domain_dimensions[0]*2**level)
-        elif ds.dimensionality:
-            ad = ds.covering_grid( level=level,
-                                left_edge=ds.domain_left_edge,
-                                dims=[ds.domain_dimensions[0]*2**level, ds.domain_dimensions[1]*2**level, 1] )
-            q = ad[quantity].to_ndarray().squeeze()
-            q = q[q.shape[0]//2:,q.shape[1]//2]
-            r = np.linspace(
-                0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0]),
-                ds.domain_right_edge[0],
-                ds.domain_dimensions[0]*2**level//2)
-            r -= 0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0])
-        return r.to_ndarray(), q, ds.current_time.to_value()
+                            dims=[ds.domain_dimensions[0]*2**level, ds.domain_dimensions[1]*2**level, 1] )
+        q = ad[quantity].to_ndarray().squeeze()
+        q = q[q.shape[0]//2:,q.shape[1]//2]
+        r = np.linspace(
+            0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0]),
+            ds.domain_right_edge[0],
+            ds.domain_dimensions[0]*2**level//2)
+        r -= 0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0])
+    return r.to_ndarray(), q
