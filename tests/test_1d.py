c0de6dead750c2a29b6f8bef50aea94ff33af254
Remi Lehe <remi.lehe@normalesup.org>
2025-10-16 12:25:25 -0700

    Add 1d DESY benchmark in test

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -51,9 +51,12 @@ def run_castro_simulation(runtime_options):
 
 def test_1d_sedov_taylor():
     """
-    Test that the 1D code produce the correct Sedov-Taylor blast wave solution.
+    Test that code produce the exact Sedov-Taylor blast wave solution, in a simplified setup:
+    - no ionization reactions (castro.add_ext_src=0)
+    - no temperature diffusion (castro.diffuse_temp=0)
+    - the initial radius of the hot plasma is small (5 microns)
     """
-    # Generate openPMD inital conditions:
+    # Generate openPMD inital conditions for a small-radius plasma
     # 1000eV plasma in the first 5 microns, low-temperature plasma in the rest
     r = np.linspace(0, 10e-6, 1024)
     T_eV = np.ones_like(r) * 1000
@@ -74,6 +77,42 @@ def test_1d_sedov_taylor():
     # TODO: Compare the results with Sedov-Taylor theory
     # Evaluate checksum
     evaluate_checksum("1d_sedov_taylor", "plt_1d_*")
+
+def test_1d_desy_benchmark():
+    """
+    Test the code in the scenario that benchmarked with DESY team
+    (close - but not identical - to the one from Mewes et al., PRR 5, 033112, 2023)
+    """
+    # Generate openPMD inital conditions according to the agreed-upon benchmark
+    sigma1 = 38e-6  # in m
+    sigma2 = 35e-6  # in m
+    Te_max = 27 # in eV
+    Ta = 0.03 # in eV
+
+    # Create r array from 0 to 6e-4 with 1e-6 increment
+    r = np.arange(0, 6e-4 + 1e-6, 1e-6)
+    # Calculate ionization fraction, with minimal ionization fraction of 1e-3
+    # (the minimal fraction is needed for the electron temperature to be defined everywhere)
+    ioniz_fraction = (1. - 1.e-3)*np.exp(-np.power(r*r/(2*sigma1*sigma1), 12)) + 1.e-3
+    # Calculate electron temperature, with a minimal temperature of 0.03 eV
+    T_eV = (Te_max - Ta) * np.exp(-np.power(r*r/(2*sigma2*sigma2), 3)) + Ta
+    # Parse the species names for which Castro has been compiled
+    with open('../sim_folder/build/species.net', 'r') as f:
+        species_keys = re.findall(r'\n\s.*\s([A-Z][a-z]*\d)', f.read())
+    populations = np.zeros((len(r), len(species_keys)))
+    # Set H0 and H1 fractions
+    populations[:, species_keys.index('H0')] = 1 - ioniz_fraction
+    populations[:, species_keys.index('H1')] = ioniz_fraction
+    # Save file
+    save_to_openpmd( {'r': [r.min(), r.max()]}, populations,
+        T_eV, '1d_desy_benchmark.h5', species_keys)
+
+    # Run the code
+    run_castro_simulation("problem.initial_conditions_file=1d_desy_benchmark.h5")
+
+    # Evaluate checksum
+    evaluate_checksum("1d_desy_benchmark", "plt_1d_*")
+
 
 if __name__ == "__main__":
     test_1d_sedov_taylor()
