4f8252039a7984780df3c6621ac2160b6657f873
tbenahmed <thibault.benahmed@gmail.com>
2025-11-02 17:54:07 +0100

    Add comparaison with ST theory for 1d test  (#68)
    * add comparaison with ST theory for 1d test
    
    * minor fix space
    
    * 1d ST theory/sim comparaison
    
    * Better way to compute the energy that ensure conservation
    
    * add checksum and cleanoutput
    
    * misc changes
    
    * misc changes
    
    * Correction with Remi comments
    
    * small changes
    
    * Syntax correction and add energy computation inside Castrosim
    
    * comments and tolerance values
    
    * mistake with title
    
    * add error output
    
    * Some cleanup
    
    * Enter explicit formula
    
    * Update tolerance
    
    ---------
    
    Co-authored-by: Thibault Jean Benahmed <benahmed@max-wgse001.desy.de>
    Co-authored-by: Thibault Jean Benahmed <thibault.benahmed@desy.de>
    Co-authored-by: Remi Lehe <remi.lehe@normalesup.org>

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -1,6 +1,5 @@
 """
 This script tests that the 1D code produce the correct Sedov-Taylor blast wave solution.
-
 It assumes that the code has already been compiled in ../sim_folder/build/
 """
 import subprocess
@@ -9,19 +8,72 @@ import numpy as np
 import sys
 import glob
 import os
+import time
 sys.path.append("../initial_condition")
 from ionization_routines import save_to_openpmd
+sys.path.append('../sim_folder/analysis/')
+from analysis_tool import CastroSimulation
+sys.path.append('../theory/sedov_theory/python/')
+from sedov_theory import SedovTalorProblem
 from checksum.checksumAPI import evaluate_checksum
+from scipy.optimize import curve_fit
+from scipy.constants import e, m_p
 
 def cleanup_outputs(extra_file = ""):
     # Remove previously generated plotfiles and checkpoints
-    os.system(f"rm -rf plt_* chk* amr_diag.out species_diag.out grid_diag.out " + extra_file)
 
+    os.system("rm -rf plt_1d_* chk* amr_diag.out species_diag.out grid_diag.out Backtrace.0" + extra_file)
+
+def load_sim():
+    cs = CastroSimulation('.', 'plt_1d_')
+    """Extract rmax for each output time."""
+    r_arr, rmax_arr, q_arr, E_tot_arr = [], [], [], []
+    t_arr = np.array(cs.output_times)
+    for t0 in t_arr:
+        r, q, t = cs.extract_data(t0, 'density', level=3)
+        rmax = r[np.argmax(q)]
+        rmax_arr.append(rmax)
+        q_arr.append(q)
+        r_arr.append(r)
+        E_tot_arr.append(cs.get_energy(t, level=3)[0])
+    return {'time': np.array(t_arr), 'r': np.array(r_arr), 'rmax': np.array(rmax_arr), 'q': np.array(q_arr), 'E_tot': np.array(E_tot_arr)}
+
+def check_energy_conservation(sim_data, tol: float = 1.0):
+    rel_err = np.abs(sim_data['E_tot'] - sim_data['E_tot'][0]) / sim_data['E_tot'][0] * 100.0
+    test = np.all(rel_err < tol)
+    value = np.max(rel_err)
+    assert test, f"Energy conservation test failed: Avg. Deviation = {value:.1e} % > {tol}% tol."
+
+def check_r_t_ST(sim_data, sol, tol: int = 10):
+    popt, _ = curve_fit(lambda t, a: a * np.sqrt(t), sim_data['time'][1:], sim_data['rmax'][1:])
+    r_fit = popt[0] * np.sqrt(sim_data['time'])
+    r_analytical = np.array(sol.blast_radius(sim_data['time']))
+    rel_error = np.linalg.norm(r_fit*1e4 - r_analytical*1e4) / np.linalg.norm(r_analytical*1e4) * 100.
+    assert rel_error < tol, f"Shock radius comparison to Sedov Taylor theory failed: rel. err. = {rel_error:.1f} % > {tol} % tol."
+
+def check_rho_r_ST(sim_data, sol, tol: int = 15):
+    indices = np.arange(0.7, 0.99, 0.05) * len(sim_data['time'])
+    errors = []
+    for idx in np.unique(indices):
+        idx = int(idx)
+        r = sim_data['r'][idx]
+        rho_sim = sim_data['q'][idx]
+        t = sim_data['time'][idx]
+    rho_analytical = sol.evaluate('density', r, t)
+
+    # compare up to the first peak present in both profiles
+    peak_idx = min(np.argmax(rho_analytical), np.argmax(rho_sim))
+    if r[peak_idx] > 1e-2: # dont compared for low blast radius
+        denom = np.linalg.norm(rho_sim[:peak_idx])
+        err = np.linalg.norm(rho_analytical[:peak_idx] - rho_sim[:peak_idx]) / denom
+        errors.append(err)
+
+    mean_rel_error = np.mean(np.array(errors)) * 100.
+    assert mean_rel_error < tol, f"Density profile comparison to Sedov Taylor theory failed: rel. err. = {mean_rel_error:.1f} % > {tol} % tol."
 
 def run_castro_simulation(runtime_options):
     """
     Run the Castro simulation.
-
     Raise an error and print stdout/stderr if the command fails.
     """
     # Find the Castro executable
@@ -52,38 +104,58 @@ def run_castro_simulation(runtime_options):
         print("STDERR:", e.stderr)
         raise
 
-
 def test_1d_sedov_taylor():
     """
     Test that code produce the exact Sedov-Taylor blast wave solution, in a simplified setup:
     - no ionization reactions (castro.add_ext_src=0)
     - no temperature diffusion (castro.diffuse_temp=0)
-    - the initial radius of the hot plasma is small (5 microns)
     """
+    print("Generating initial conditions...")
     # Generate openPMD inital conditions for a small-radius plasma
-    # 1000eV plasma in the first 5 microns, low-temperature plasma in the rest
+    # Gaussian temperature profile with sigma=4 microns, peak T=1000 eV
     r = np.linspace(0, 10e-6, 1024)
-    T_eV = np.ones_like(r) * 1000
+    sigma = 4e-6
+    T0_eV = 1000
+    T_eV = np.ones_like(r) * T0_eV * np.exp(-r**2/sigma**2) # Gaussian profile to fasten convergence
     # Parse the species names for which Castro has been compiled
     with open('../sim_folder/build/species.net', 'r') as f:
         species_keys = re.findall(r'\n\s.*\s([A-Z][a-z]*\d)', f.read())
     populations = np.zeros((len(r), len(species_keys)))
     # Set fraction to 1 for H+
-    populations[:, species_keys.index('H1')] = 1
+    populations[:, species_keys.index('H1')] = 1 - 1e-3
+    populations[:, species_keys.index('H0')] = 1e-3
     # Save file
     save_to_openpmd( {'r': [r.min(), r.max()]}, populations,
         T_eV, '1d_sedov_taylor.h5', species_keys)
 
     # Run the code
+    print("Starting simulation...")
+    time_s = time.time()
     run_castro_simulation("castro.add_ext_src=0 castro.diffuse_temp=0 problem.initial_conditions_file=1d_sedov_taylor.h5")
+    time_e = time.time()
+    print(f"Simulation completed in {time_e - time_s:.2f} seconds.")
+    # Physical tests #
+    print("Running physical tests...\n")
+    sim_data = load_sim()
 
-    # Check the results
-    # TODO: Compare the results with Sedov-Taylor theory
+    # Comparison with Sedov Taylor theory
+    rho_initial = 1.67e-6  # in g.cm^-3
+    mp_g = m_p*1e3 # in g
+    sigma_cm = sigma*1e2 # in cm
+    deposited_energy = 3*np.pi/2 * T0_eV*e * sigma_cm**2 * rho_initial/mp_g * 1e7 # in erg/cm (computed by integrating initial conditions)
+    analytical_data = SedovTalorProblem(5.0 / 3.0, deposited_energy, rho_initial)
+
+    check_energy_conservation(sim_data, tol=1.0)
+    check_r_t_ST(sim_data, analytical_data, tol=10)
+    check_rho_r_ST(sim_data, analytical_data, tol=22)
+
+    print("Physical tests passed.\n")
     # Evaluate checksum
     evaluate_checksum("1d_sedov_taylor", "plt_1d_*")
 
     # Remove generated plotfiles and checkpoints
     cleanup_outputs('1d_sedov_taylor.h5')
+
 
 def test_1d_desy_benchmark():
     """
@@ -95,7 +167,6 @@ def test_1d_desy_benchmark():
     sigma2 = 35e-6  # in m
     Te_max = 27 # in eV
     Ta = 0.03 # in eV
-
     # Create r array from 0 to 6e-4 with 1e-6 increment
     r = np.arange(0, 6e-4 + 1e-6, 1e-6)
     # Calculate ionization fraction, with minimal ionization fraction of 1e-3
@@ -116,13 +187,11 @@ def test_1d_desy_benchmark():
 
     # Run the code
     run_castro_simulation("problem.initial_conditions_file=1d_desy_benchmark.h5")
-
     # Evaluate checksum
     evaluate_checksum("1d_desy_benchmark", "plt_1d_*")
 
     # Remove generated plotfiles and checkpoints
     cleanup_outputs('1d_desy_benchmark.h5')
-
 
 if __name__ == "__main__":
     test_1d_sedov_taylor()
