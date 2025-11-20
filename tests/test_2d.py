c1a86a104e7cc10565b12bd0554af8365ea1d330
Thibault Jean Benahmed <thibault.benahmed@desy.de>
2025-11-20 11:26:48 +0100

    update test2d with the new Castrosim

--- tests/test_2d.py
+++ tests/test_2d.py
@@ -28,46 +28,14 @@ def cleanup_outputs(extra_file = ""):
     # Remove previously generated plotfiles and checkpoints
     os.system(f"rm -rf plt_2d_* chk* amr_diag.out species_diag.out grid_diag.out Backtrace.0 " + extra_file)
 
-def load_sim():
-        # List all directories that match your pattern
-        folders = sorted(glob.glob("./plt_2d_*"))
-        folders = [f for f in folders if os.path.exists(os.path.join(f, "Header"))]
-        # Field to plot
-        field_rho, field_T = ("boxlib", "density"), ("boxlib", "Temp")
-        time, data_rho, data_T = [], [], []
-        for folder in folders:  
-            ds = yt.load(folder)
-
-            time.append(float(ds.current_time))
-            # Extract the full 2D grid
-            cg = ds.covering_grid(
-                level=0,
-                left_edge=ds.domain_left_edge,
-                dims=ds.domain_dimensions
-            )
-
-            # Convert field to 2D array
-            array_2d = cg[field_rho].to_ndarray()
-            array_2d = np.squeeze(array_2d)  # remove singleton dimensions
-            data_rho.append(array_2d)
-
-            # Extract temperature field
-            array_2d_T = cg[field_T].to_ndarray()
-            array_2d_T = np.squeeze(array_2d_T)  # remove singleton dimensions
-            data_T.append(array_2d_T)
-        sim_data = {'time': np.array(time),
-                    'density': np.array(data_rho),
-                    'temperature': np.array(data_T)}
-        return sim_data
-
 def check_r_iso_t(sim_data, sol, tol_r: int = 10, tol_iso: float = 0.5):
 
     def find_edge_radial_xy(data, n_angles=100, n_samples=1000):
-        x, y = np.linspace(0, 100, data.shape[1]), np.linspace(0, 100, data.shape[0])
-        cx, cy = 50, 50
+        x, y = np.array(data['x'], dtype=float), np.array(data['y'], dtype=float)
+        cx, cy = x[-1]/2, y[-1]/2
 
         # create interpolator on physical grid
-        interp = RegularGridInterpolator((y, x), data, bounds_error=False, fill_value=np.nan)
+        interp = RegularGridInterpolator((y, x), data['q'], bounds_error=False, fill_value=np.nan)
 
         # radial sampling
         thetas = np.linspace(0, 2*np.pi, n_angles, endpoint=False)
@@ -101,23 +69,24 @@ def check_r_iso_t(sim_data, sol, tol_r: int = 10, tol_iso: float = 0.5):
 
         return x_edge, y_edge
 
-    def fit_circle_radius(x, y, R0 = 50):
+    def fit_circle_radius(x, y, data):
+        R0 = np.array(data['x'][-1], dtype=float)/2
         r = np.sqrt((x-R0)**2 + (y-R0)**2)
         R = np.mean(r)
         iso = np.std(r)/np.mean(r)
         return R, iso
     
-    L_time, L_r, L_r_analytical, L_iso = [], [], [], []
-    for idx in range(1, len(sim_data['time'])):
-        data_fit = sim_data['density'][idx]
+    L_r, L_r_analytical, L_iso = [], [], []
+    t_sim = sim_data.output_times
+    for t_ in t_sim:
+        data_fit = sim_data.get_field(t_, quantity ='density', level=2)
         x_max, y_max = find_edge_radial_xy(data_fit)
-        R_fit, iso = fit_circle_radius(x_max, y_max)
-        r_analytical = sol.blast_radius(sim_data['time'][idx])
-        L_time.append(sim_data['time'][idx])
+        R_fit, iso = fit_circle_radius(x_max, y_max, data_fit)
+        r_analytical = sol.blast_radius(t_)
         L_r.append(R_fit)
         L_r_analytical.append(r_analytical)
         L_iso.append(iso)
-    mask = np.array(L_time) >= 1e-9 # avoid early times with poor resolution
+    mask = np.array(t_sim) >= 1e-9 # avoid early times with poor resolution
     error_r = np.linalg.norm(np.array(L_r)[mask] - np.array(L_r_analytical)[mask]) / np.linalg.norm(np.array(L_r_analytical)[mask]) * 100.
     error_iso = np.mean(np.array(L_iso)[mask])
     test_r = error_r < tol_r
@@ -126,74 +95,26 @@ def check_r_iso_t(sim_data, sol, tol_r: int = 10, tol_iso: float = 0.5):
     assert test_r, f"Shock radius test failed: rel. error = {error_r:.2f} % > {tol_r} %"
     assert test_iso, f"Shock isotropy test failed: mean isotropy = {error_iso:.2f} % > {tol_iso} %"
 
-def check_energy_conservation(tol: float = 1.0):
-    """
-    Extract the total energy (thermal + kinetic + potential) as a function of time in 2D Cartesian geometry.
-    The result is given per unit length along the z direction (J/m).
-    """
-    E_kin_thermal = []
-    E_pot = []
-    E_tot = []
-    time = []
-    yt_timeseries = yt.load('plt_2d_*')
+def check_energy_conservation(sim_data, tol: float = 1.0):
+    t = sim_data.output_times
+    E_tot = sim_data.get_energy(t, level=2, energy_type='total')[0]
+    rel_err = np.abs(E_tot - E_tot[0]) / E_tot[0] * 100.0
+    test = np.all(rel_err < tol)
+    value = np.max(rel_err)
+    assert test, f"Energy conservation test failed: Avg. Deviation = {value:.1e} % > {tol}% tol."
 
-    for ds in yt_timeseries:
-        ad0 = ds.covering_grid(level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
-
-        # Coordinates (in meters)
-        x = np.linspace(ds.domain_left_edge[0], ds.domain_right_edge[0], ds.domain_dimensions[0])
-        y = np.linspace(ds.domain_left_edge[1], ds.domain_right_edge[1], ds.domain_dimensions[1])
-        dx = (x[1] - x[0])
-        dy = (y[1] - y[0])
-
-        # Energy density [erg/cm^3] -> [J/m^3]
-        E = ad0['rho_E'].to_ndarray() * 1e-7 * 1e6
-        e_kin_thermal = np.sum(E) * dx * dy  # J/m
-
-        # Hydrogen density [g/cm^3] -> [kg/m^3]
-        rho_Hp = ad0['rho_H1'].to_ndarray() * 1e-3 * 1e6
-        n_e = rho_Hp / m_p
-        Ntot = np.sum(n_e) * dx * dy  # particles/m
-        e = 1.60218e-19  # C
-        # Potential energy (13.6 eV per electron)
-        e_pot = 13.6 * e * Ntot  # J/m
-
-        E_kin_thermal.append(e_kin_thermal)
-        E_pot.append(e_pot)
-        E_tot.append(e_kin_thermal + e_pot)
-        time.append(float(ds.current_time))
-
-    # Convert to arrays
-    time = np.array(time)
-    E_kin_thermal = np.array(E_kin_thermal) * 1e3  # mJ/m
-    E_pot = np.array(E_pot) * 1e3  # mJ/m
-    E_tot = np.array(E_tot) * 1e3  # mJ/m
-
-    # Check energy conservation
-    rel_error = np.max(np.abs(E_tot - E_tot[0]) / np.abs(E_tot[0]) * 100.)
-    test = rel_error < tol
-    assert test, f"Energy conservation test failed: Max. Deviation = {rel_error:.2f} % > {tol} %"
-
-def check_rho_r(sim_data, sol, tol: int = 16):
+def check_rho_r(sim_data, sol, tol: int = 21):
     """
     Compare radial density profiles at several output times to the analytical solution.
     Returns True if the mean relative L2 error is below 15%.
     """
-    time_idx = np.arange(0.6, 0.99, 0.1) * len(sim_data['time'])
-    r_binned_all, data_binned_all, r_analytical_all, data_analytical_all = [], [], [], []
-    for idx in time_idx:
-        idx = int(idx)
-        x = np.linspace(-50, 50, sim_data['density'][idx].shape[0])
-        rho_fit_center = sim_data['density'][idx][:, sim_data['density'][idx].shape[0]//2]
-        rho_analytical = sol.evaluate('density', np.abs(x), sim_data['time'][idx])
-        r_binned_all.append(x)
-        data_binned_all.append(rho_fit_center)
-        r_analytical_all.append(x)
-        data_analytical_all.append(rho_analytical)
     errors = []
-    for k in range(len(time_idx)):
-        rho_sim = data_binned_all[k]
-        rho_analytical = data_analytical_all[k]
+    t_sim = sim_data.output_times
+    for t_ in t_sim[3::2]:
+        m = sim_data.get_field(t_, quantity='density', level=2)
+        rho_sim = m['q'][:, m['q'].shape[0]//2]
+        x_center = np.linspace(-np.array(m['x'], dtype = float)[len(m['x'])//2], np.array(m['x'], dtype = float)[len(m['x'])//2], len(m['x']), dtype=float)
+        rho_analytical = sol.evaluate('density', np.abs(x_center), t_)
         # compare up to the first peak present in both profiles
         peak_idx = min(np.argmax(rho_analytical), np.argmax(rho_sim))
         denom = np.linalg.norm(rho_sim[:peak_idx])
@@ -244,7 +165,6 @@ def test_2d_sedov_taylor():
     - no ionization reactions (castro.add_ext_src=0)
     - no temperature diffusion (castro.diffuse_temp=0)
     """
-    print("Generating initial conditions...")
     # Grid
     x = np.linspace(0.0, 600e-6, 256)
     y = np.linspace(0.0, 600e-6, 256)
@@ -260,24 +180,29 @@ def test_2d_sedov_taylor():
         species_keys = re.findall(r'\n\s.*\s([A-Z][a-z]*\d)', f.read())
 
     # Populations array
+    f_ion = 1.0  # ionization fraction
     populations = np.zeros((X.shape[0], X.shape[1], len(species_keys)))
-    populations[:, :, species_keys.index('H1')] = 1.0 - 1e-3
-    populations[:, :, species_keys.index('H0')] = 1e-3
+    populations[:, :, species_keys.index('H1')] = f_ion - 1e-3
+    populations[:, :, species_keys.index('H0')] = f_ion
     # Save file
     save_to_openpmd({'x': [x.min(), x.max()], 'y': [y.min(), y.max()]},
                 populations, T_eV, '2d_sedov_taylor.h5', species_keys)
-    print("Starting simulation...")
     # Run the code
-    run_castro_simulation("amr.n_cell = 64   64 problem.initial_conditions_file=2d_sedov_taylor.h5")
-    # Physical tests
-    deposited_energy = 1.19e16 # in erg/cm
+    run_castro_simulation("amr.n_cell = 64 64 castro.add_ext_src = 0 castro.diffuse_temp = 0 amr.max_level  = 3 problem.initial_conditions_file=2d_sedov_taylor.h5")
+    # Physical tests 
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+    dA_cm2 = dx * dy * 1e1
+    eV_to_erg = 1.60218e-12
     rho_initial = 1.67e-6  # in g/cm^3
-    sim_data = load_sim()
+    deposited_energy = 3/2 * rho_initial / m_p * f_ion * eV_to_erg * np.sum(T_eV) * dA_cm2
+    rho_initial = 1.67e-6  # in g/cm^3
+    sim_data = CastroSimulation('.', 'plt_2d_')
     analytical_data = SedovTalorProblem(5.0 / 3.0, deposited_energy, rho_initial) # E0 in mJ/m, rho_0 in g/cm^3 (computed by integrating the initial profile of temperature ponderated by the populations)
 
     check_r_iso_t(sim_data, analytical_data, tol_r=10, tol_iso=0.5)
-    check_energy_conservation(tol=1.0)
-    check_rho_r(sim_data, analytical_data, tol=16)
+    check_energy_conservation(sim_data, tol=1.0)
+    check_rho_r(sim_data, analytical_data, tol=21)
 
     # Evaluate checksum
     evaluate_checksum("2d_sedov_taylor", "plt_2d_*")
